135e5bdc1d30bed1b94527de2bfb8180e2c39c5f
rudyjm3 <rudyjm3@yahoo.com>
2020-08-23 20:11:04 -0400

    preloader jf function and html added

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 // MDN
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
@@ -16,3 +15,10 @@ btn.addEventListener('click', function(){
     video.play();
   }
 });
+
+// Prealoder
+const preloader = document.querySelector('.preloader');
+
+window.addEventListener('load',function(){
+  preloader.classList.add("hide-preloader");
+})
